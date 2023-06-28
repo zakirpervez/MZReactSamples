9a41d75e53202793ae1a779ab155c6e5027c35d2
Zakir.Mohammad <zmohammad@schoology.com>
2023-06-28 16:05:25 +0530

    added animal and heart image

--- animals/src/AnimalShow.js
+++ animals/src/AnimalShow.js
@@ -1,6 +1,28 @@
+import bird from './assets/svg/bird.svg';
+import cat from './assets/svg/cat.svg';
+import cow from './assets/svg/cow.svg';
+import dog from './assets/svg/dog.svg';
+import gator from './assets/svg/gator.svg';
+import heart from './assets/svg/heart.svg';
+import horse from './assets/svg/horse.svg';
+import {useState} from "react";
 
-function AnimalShow({ type, key }) {
-    return <div>{ type }</div>
+const svgMap = {
+    bird, cat, cow, gator, horse, dog, heart
+}
+
+
+
+function AnimalShow({ type }) {
+    const [clicks, setClicks] = useState(0)
+    const handleClickEvent = () => {
+        setClicks(clicks+1)
+    };
+
+    return <div onClick={handleClickEvent}>
+        <img alt="animal" src={svgMap[type]}/>
+        <img alt="heart" src={heart} style={{width: 10+10*clicks+'px'}}/>
+    </div>
 }
 
 export default AnimalShow;
