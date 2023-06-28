5fec3ff721dce5ebf560e3166d7c00fbaca3db52
Zakir.Mohammad <zmohammad@schoology.com>
2023-06-28 15:33:08 +0530

    added random animal using array

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,13 +1,21 @@
 import AnimalShow from "./AnimalShow";
 import { useState } from "react";
 
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'dog', 'gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
-    const [count, setCount] = useState(0)
+    const [animals, setAnimals] = useState([]);
+
+    const handleClickEvent = () => {
+        setAnimals([...animals, getRandomAnimal()]);
+    };
+
     return <div>
-        <div><button onClick={() => {
-            setCount(count+1)
-        }}>Add Animal</button>
-        <div>Number of animal is {count}</div>
+        <div><button onClick={handleClickEvent}>Add Animal</button>
+        <div>Number of animal is {animals}</div>
         </div>
     </div>
 }
