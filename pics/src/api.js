c7ef00bb5a9d9ddedf7b8e71030c0dc5febcf05c
Zakir.Mohammad <zmohammad@schoology.com>
2023-07-03 17:27:09 +0530

    added api call using axios

---
+++ pics/src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const searchImages = async (term) => {
+  const response = await axios.get('https://api.unsplash.com/search/photos', {
+     headers: {
+        Authorization: 'Client-ID -BC5FGBbngxpRrvNe3bn2M9fcyslUV5IUjnOeGPGB7o'
+     },
+     params: {
+        query: term
+     }
+  });
+  console.log(response);
+  return response.data.results;
+};
+
+export default searchImages;
