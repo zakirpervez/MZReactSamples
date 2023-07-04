bde9a0f7d2bb0ce8734c19188150a40b50eb6777
Zakir.Mohammad <zmohammad@schoology.com>
2023-07-04 15:30:43 +0530

    added searchbar component code

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,19 @@
-function SearchBar() {
+import {useState} from "react";
 
+function SearchBar({ onSubmit }) {
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(query)
+    };
+    const [query, setQuery] = useState('')
+
+    return <div>
+        <form onSubmit={ handleFormSubmit }>
+            <input  value={ query } onChange={ (event) => {
+                setQuery(event.target.value)
+            } }/>
+        </form>
+    </div>;
 }
 
 export default SearchBar;
