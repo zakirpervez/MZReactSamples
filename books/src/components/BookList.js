28fc2d884a6ae10ec486ae151566093b336b42d6
Zakir.Mohammad <zmohammad@schoology.com>
2023-07-21 18:46:35 +0530

    refactored unwanted code

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -3,16 +3,12 @@ import BooksContext from "../context/Books";
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
-    const {count, incrementCount} = useContext(BooksContext)
-
     const renderedBooks = books.map((book) => {
         return <BookShow key={ book.id } book={ book } onDelete={ onDelete } onEdit={ onEdit }/>
     });
 
     return (
         <div className="book-list">
-            <h1>{ count }</h1>
-            <button onClick={ incrementCount }>Click</button>
             { renderedBooks }
         </div>
     );
