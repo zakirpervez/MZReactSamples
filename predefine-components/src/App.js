435af77b413e31cb1d50a86295d723d70dd1d056
Zakir.Mohammad <zmohammad@schoology.com>
2023-09-11 15:33:28 +0530

    prederine project is loaded

---
+++ predefine-components/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          This is react components.
+      </header>
+    </div>
+  );
+}
+
+export default App;
