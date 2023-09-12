f380a3b67c45c997a219df71752c96be9f20587f
Zakir.Mohammad <zmohammad@schoology.com>
2023-09-12 15:43:26 +0530

    classnames and react-icons libraries added and button code modified

--- predefine-components/src/App.js
+++ predefine-components/src/App.js
@@ -1,13 +1,16 @@
 import Button from "./mcomponents/Button";
+import {GoBell, GoCode} from "react-icons/go";
 
 function App() {
   return (
     <div className="App">
-        <Button success={true} >Click me</Button>
-        <Button >Button 1</Button>
-        <Button >Button 2</Button>
-        <Button >Button 3</Button>
-        <Button >Button 4</Button>
+        <div style={{ margin: 10}}><Button primary={true} rounded={true}> <GoBell />Primary Button</Button></div>
+        <div style={{ margin: 10}}><Button  secondary={true}><GoCode />Secondary Button</Button></div>
+        <div style={{ margin: 10}}><Button success={true}>Success Button</Button></div>
+        <div style={{ margin: 10}}><Button warning={true}>Warning Button</Button></div>
+        <div style={{ margin: 10}}><Button danger={true}>Danger Button</Button></div>
+        <div style={{ margin: 10}}><Button rounded={true}>Rounded Button</Button></div>
+        <div style={{ margin: 10}}><Button  primary={true} outline={true}>Outline Button</Button></div>
     </div>
   );
 }
--- predefine-components/src/mcomponents/Button.js
+++ predefine-components/src/mcomponents/Button.js
@@ -1,23 +1,36 @@
-import PropTypes from "prop-types";
+import classNames from "classnames";
 
-
+// const finalClassName = classNames('px-3', 'py-1.5', 'border');
+// const finalClassName = classNames('px-3', 'py-1.5', 'border', {
+//     'border-blue-600': true,
+//     'text-yellow-500': false
+// });
+// console.log(finalClassName)
 
 function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
-    return <button className={ "bg-blue-300" }>{ children }</button>
+    const classes = classNames('px-3 py-1.5 border flex item-center', {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-500 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-900 text-white': success,
+        'border-yellow-400 bg-yellow-500 text-white': warning,
+        'border-red-400 bg-red-500 text-white': danger,
+        'rounded-full': rounded,
+        'bg-white': outline,
+        'text-blue-500': outline && primary,
+    });
+    return <button className={ classes }>{ children }</button>
 }
 
 Button.propTypes = {
-    checkVariationValues: ({ primary, secondary, success, warning, danger, outline, rounded}) => {
+    checkVariationValues: ({ primary, secondary, success, warning, danger}) => {
         const count = Number(!!primary)
             + Number(!!secondary)
             + Number(!!success)
             + Number(!!warning)
-            + Number(!!danger)
-            + Number(!!outline)
-            + Number(!!rounded);
+            + Number(!!danger);
 
         if (count>1) {
-            return Error('Only one type of button would be allowed {primary, secondary, success, warning, danger, outline, rounded}');
+            return Error('Only one type of button would be allowed {primary, secondary, success, warning, danger}');
         }
     }
 }
