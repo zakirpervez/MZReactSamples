0b4ea65677cdcb551cd2ffb729003b7e8e192de3
Zakir.Mohammad <zmohammad@schoology.com>
2023-09-12 16:23:21 +0530

    button work completed

--- predefine-components/src/App.js
+++ predefine-components/src/App.js
@@ -2,15 +2,18 @@ import Button from "./mcomponents/Button";
 import {GoBell, GoCode} from "react-icons/go";
 
 function App() {
+    const handleClick = () => {
+        console.log("Button clicked");
+    };
   return (
     <div className="App">
-        <div style={{ margin: 10}}><Button primary={true} rounded={true}> <GoBell />Primary Button</Button></div>
-        <div style={{ margin: 10}}><Button  secondary={true}><GoCode />Secondary Button</Button></div>
-        <div style={{ margin: 10}}><Button success={true}>Success Button</Button></div>
-        <div style={{ margin: 10}}><Button warning={true}>Warning Button</Button></div>
-        <div style={{ margin: 10}}><Button danger={true}>Danger Button</Button></div>
-        <div style={{ margin: 10}}><Button rounded={true}>Rounded Button</Button></div>
-        <div style={{ margin: 10}}><Button  primary={true} outline={true}>Outline Button</Button></div>
+        <div ><Button primary={true} rounded={true} onClick={handleClick} className="mb-5"> <GoBell />Primary Button</Button></div>
+        <div><Button  secondary={true} onMouseEnter={handleClick} className="mb-5"><GoCode />Secondary Button</Button></div>
+        <div ><Button success={true} onMouseLeave={handleClick} className="mb-5">Success Button</Button></div>
+        <div ><Button warning={true} className="mb-5">Warning Button</Button></div>
+        <div ><Button danger={true} className="mb-5">Danger Button</Button></div>
+        <div ><Button rounded={true} className="mb-5">Rounded Button</Button></div>
+        <div ><Button  primary={true} outline={true} className="mb-5">Outline Button</Button></div>
     </div>
   );
 }
--- predefine-components/src/mcomponents/Button.js
+++ predefine-components/src/mcomponents/Button.js
@@ -7,8 +7,8 @@ import classNames from "classnames";
 // });
 // console.log(finalClassName)
 
-function Button({ children, primary, secondary, success, warning, danger, outline, rounded }) {
-    const classes = classNames('px-3 py-1.5 border flex item-center', {
+function Button({ children, primary, secondary, success, warning, danger, outline, rounded, ...rest}) {
+    const classes = classNames(rest.className ,'px-3 py-1.5 border flex item-center', {
         'border-blue-500 bg-blue-500 text-white': primary,
         'border-gray-500 bg-gray-900 text-white': secondary,
         'border-green-500 bg-green-900 text-white': success,
@@ -16,9 +16,9 @@ function Button({ children, primary, secondary, success, warning, danger, outlin
         'border-red-400 bg-red-500 text-white': danger,
         'rounded-full': rounded,
         'bg-white': outline,
-        'text-blue-500': outline && primary,
+        'text-blue-600': outline && primary,
     });
-    return <button className={ classes }>{ children }</button>
+    return <button {...rest} className={ classes }>{ children }</button>
 }
 
 Button.propTypes = {
